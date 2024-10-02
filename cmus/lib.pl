c5929cc4f019d265bed4d11df6a3fd853d483d11
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-10-02 00:23:54 -0300

    archivos de configuracion

---
+++ cmus/lib.pl
@@ -0,0 +1,38 @@
+/home/luichi/Música/japones/【Ado】Overdose 歌いました.mp3
+/home/luichi/Música/pop/AJR - World’s Smallest Violin.mp3
+/home/luichi/Música/imagine dragons/Imagine Dragons - Bones.mp3
+/home/luichi/Música/japones/YOASOBI「怪物」(YOASOBI - Monster).mp3
+/home/luichi/Música/japones/BLOODY STREAM.mp3
+/home/luichi/Música/español/Insaciable.mp3
+/home/luichi/Música/español/El hijo de Hernández.mp3
+/home/luichi/Música/español/Malherido.mp3
+/home/luichi/Música/español/Mírenme.mp3
+/home/luichi/Música/español/Lo malo de ser bueno.mp3
+/home/luichi/Música/español/La Bestia.mp3
+/home/luichi/Música/japones/Stay With Me - Miki Matsubara .mp3
+/home/luichi/Música/imagine dragons/Imagine Dragons - Sharks.mp3
+/home/luichi/Música/japones/ファティマ.mp3
+/home/luichi/Música/imagine dragons/Imagine Dragons - I'm So Sorry.mp3
+/home/luichi/Música/japones/Peace Sign.mp3
+/home/luichi/Música/japones/Sousou no Frieren | Yuusha.mp3
+/home/luichi/Música/japones/ADAMAS.mp3
+/home/luichi/Música/pop/Born for this • The Score.mp3
+/home/luichi/Música/pop/Legend  • The Score.mp3
+/home/luichi/Música/imagine dragons/Imagine Dragons - Natural  .mp3
+/home/luichi/Música/pop/Stronger • The Score.mp3
+/home/luichi/Música/japones/катя лель - мой мармеладный  Sub Español || Katya Lel - My Marmalade  Lyrics [_Eo4DBw7i2s].mp3
+/home/luichi/Música/japones/アイデンティティ.mp3
+/home/luichi/Música/japones/美波「カワキヲアメク」.mp3
+/home/luichi/Música/japones/Kick Back OP.1 | Chainsaw Man.mp3
+/home/luichi/Música/japones/「Q」- Calliope Mori x Gawr Gura x DECO*27.mp3
+/home/luichi/Música/imagine dragons/Imagine Dragons - Gods Don't Pray.mp3
+/home/luichi/Música/español/Bugambilia.mp3
+/home/luichi/Música/pop/David Bowie - Starman.mp3
+/home/luichi/Música/japones/Wild Side - Beastars Opening 1.mp3
+/home/luichi/Música/japones/Oshi no Ko | YOASOBI - IDOL.mp3
+/home/luichi/Música/japones/tuki.『ひゅるりらぱっぱ』.mp3
+/home/luichi/Música/pop/Abba - Lay All Your Love On Me.mp3
+/home/luichi/Música/japones/Live in Life - Will Stetson【Hu Tao Fansong】.mp3
+/home/luichi/Música/japones/ケタチガイ『無礼King』 .mp3
+/home/luichi/Música/japones/ケタチガイ『Cypher』.mp3
+/home/luichi/Música/japones/ケタチガイ『お気楽Luck Life』.mp3
