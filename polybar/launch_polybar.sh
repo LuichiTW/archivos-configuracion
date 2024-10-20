17167db141a10a4abeffd0f774b31d8650181d93
LuichiTW <ltitaewatanabe@frba.utn.edu.ar>
2024-10-20 20:48:24 -0300

    dividir la barra de polybar en 3

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -1,7 +1,11 @@
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top1 &
+    MONITOR=$m polybar --reload top2 &
     MONITOR=$m polybar --reload toph &
   done
 else
   polybar --reload toph &
+  polybar --reload top1 &
+  polybar --reload top2 &
 fi
